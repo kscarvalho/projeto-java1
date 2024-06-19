ab78296b93b83c23dc0a2518212aef6fdea79e8e
cli <cli@DESKTOP-SVPJEK1>
2024-06-19 15:36:59 -0300

    variavel local e glbal

--- primeiro-programa-java/src/cursojava/executavel/PrimeiraClasseJava.java
+++ primeiro-programa-java/src/cursojava/executavel/PrimeiraClasseJava.java
@@ -2,11 +2,29 @@ package cursojava.executavel;
 
 public class PrimeiraClasseJava {
 	
-	/*main é um auto executave*/
+	static int maiorIdadeGlobal = 30;
+	
+	/*main é um auto executavel*/
 	public static void main(String[] args) {
 		
 		/*Escreve no console*/
 		System.out.println("Hello Mundo");
+		
+		 
+		
+		/*vareavel local porque pertence somente a esse metodo*/
+		int maiorIdade = 18;
+		
+		System.out.println("Maior idade é igual " + maiorIdade + " anos");
+		
+		metodo2();
+	
+	}
+	
+	public static void metodo2 () {
+		
+		System.out.println("valor da variavel Global = " + maiorIdadeGlobal );
+		
 	}
 		
 } 
