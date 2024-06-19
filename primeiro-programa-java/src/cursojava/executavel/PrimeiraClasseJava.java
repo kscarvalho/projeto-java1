fa482ecdf5be7928220de0df7ddb14e56547be43
cli <cli@DESKTOP-SVPJEK1>
2024-06-18 23:23:12 -0300

    add L

--- primeiro-programa-java/src/cursojava/executavel/PrimeiraClasseJava.java
+++ primeiro-programa-java/src/cursojava/executavel/PrimeiraClasseJava.java
@@ -2,7 +2,7 @@ package cursojava.executavel;
 
 public class PrimeiraClasseJava {
 	
-	/*main é um auto executave*/
+	/*main é um auto executavel*/
 	public static void main(String[] args) {
 		/*Escreve palavra no console*/
 		System.out.println("Meu Primeiro Programa Java");
