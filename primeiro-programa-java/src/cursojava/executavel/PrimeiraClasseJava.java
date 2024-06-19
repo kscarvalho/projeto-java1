0d99828636894167daa754fe4f6b5a66aa8b88d2
cli <cli@DESKTOP-SVPJEK1>
2024-06-18 21:42:39 -0300

    criação do projeto

---
+++ primeiro-programa-java/src/cursojava/executavel/PrimeiraClasseJava.java
@@ -0,0 +1,16 @@
+package cursojava.executavel;
+
+public class PrimeiraClasseJava {
+	
+	/*main é um auto executavel*/
+	public static void main(String[] args) {
+		/*Escreve palavra no console*/
+		System.out.println("Meu Primeiro Programa Java");
+	}
+		
+			
+} 
+			
+
+
+
