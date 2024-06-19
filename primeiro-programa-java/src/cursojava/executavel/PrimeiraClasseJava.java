700dfb1da1e8b19b5dd654d6ed8dca8ca0260268
cli <cli@DESKTOP-SVPJEK1>
2024-06-19 12:49:31 -0300

    tarefa_1_branch_1

--- primeiro-programa-java/src/cursojava/executavel/PrimeiraClasseJava.java
+++ primeiro-programa-java/src/cursojava/executavel/PrimeiraClasseJava.java
@@ -5,7 +5,7 @@ public class PrimeiraClasseJava {
 	/*main é um auto executave*/
 	public static void main(String[] args) {
 		/*Escreve instavel*/
-		System.out.println("Meu Primeiro");
+		System.out.println("alo mundo");
 	}
 		
 			
